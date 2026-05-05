d45c152aa6caddd5b22f236f475f965e6375e4fc
Sharifu Kasimba <your-sharifukasimbaidipnebosh@gmail.com>
2026-05-05 10:13:39 +0400

    Fix package.json after merge conflicts

--- routes/uploadRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/uploadController");
-
-router.post("/", controller.uploadFile);
-router.get("/template", controller.downloadTemplate);
-
-module.exports = router;
--- services/validationService.js
+++
@@ -1,5 +0,0 @@
-exports.validateRow = (r) => {
-  if (!r.Flight_Number) return "Missing Flight Number";
-  if (!r.Date) return "Missing Date";
-  return null;
-};
